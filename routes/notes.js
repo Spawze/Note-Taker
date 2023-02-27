58c6a851b375e67f1be40eba8eeb2168d96949c1
Riley Allred <riley_allred@hotmail.com>
2023-02-27 15:24:18 -0700

    added delete request functionality :)

--- routes/notes.js
+++ routes/notes.js
@@ -19,7 +19,10 @@ app.post('/notes',(req,res)=>{
     let requestData = req.body;
     requestData.id = uuid()
     fs.readFile('./db/db.json',(err,data)=>{
-        if(err){console.info("Error reading db.json\n"+err)}
+        if(err){
+            console.info("Error reading db.json\n"+err)
+            res.status(500).json("Error reading database")
+        }
         else{
             const parsedData = JSON.parse(data)
             console.log(parsedData)
@@ -27,7 +30,48 @@ app.post('/notes',(req,res)=>{
             parsedData.push(requestData)
 
             fs.writeFile('./db/db.json',JSON.stringify(parsedData,null,4), (err)=>{
-                err ? console.error(err) : console.info("Data written to /db/db.json")
+                if(err){
+                    console.error(err)
+                    res.status(500).json("Error writing to database\n"+err)
+                }else{
+                    console.info("Data written to /db/db.json")
+                    res.status(201).json("Successfully wrote note to database")
+                } 
+            })
+
+
+        }
+    })
+})
+app.delete('/notes/:id',(req,res)=>{
+    //takes the id in the delete request and assigns it to a variable
+    let idToDelete = req.params.id
+
+    fs.readFile('./db/db.json',(err,data)=>{
+        if(err){console.info("Error reading db.json\n"+err)}
+        else{
+
+            //parses database
+            db = JSON.parse(data)
+            let indexToDelete;
+            //compares the id from delete request to all of the notes in the db
+            for (let i =0; i < db.length; i++) {
+                if(idToDelete === db[i].id){
+                    indexToDelete = i;
+                    break
+                }
+            }
+            console.log(`Index to delete: ${indexToDelete}`)
+            db.splice(indexToDelete,1)
+
+            fs.writeFile('./db/db.json',JSON.stringify(db,null,4),(err)=>{
+                if(err){
+                    res.status(500).json("Error reading database")
+                    console.log(err)
+                }else{
+                    res.status(201).json("Successfully deleted note from database")
+                    console.info(`Removed note at index ${indexToDelete}`)
+                }
             })
         }
     })
